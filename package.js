c949e3d5deb5762476d87e5bfc1f09531664c333
dououFullstack <13412133311@139.com>
2015-10-22 18:39:14 +0800

    version 0.0.4

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Maodou.io activities package, A meteor package that provides activities pages at /activities',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/dououFullstack/myapp',
+  git: 'https://github.com/dououFullstack/doudouapp',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
